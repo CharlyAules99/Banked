e808ead2bf1ca9c8eb852dee88511d82f5919a23
Carlos <ContacAules@web.com>
2021-01-19 13:52:03 -0600

    Mi primer commit

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
-console.log("STAR PLATINUM!! ZA WARDO!!!!")
+const express = require('express');
+require('dotenv').config();
+
+//crear servidor Express
+const app =express();
+
+//Acceso al directorio publico
+app.use(express.static('Public'));
+
+//rutas
+app.use('/api/auth', require('./Routes/auth.js'));
+
+//configuracion del puerto
+app.listen(process.env.PORT, ()=>{
+ console.log(`Bancked corriendo en el puerto ${process.env.PORT}`);   
+})
